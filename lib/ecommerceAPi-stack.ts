cb6ee452b4b58e39ab89b30a6c647d905cdb7b82
Lucas Gonçalves <lucas.floripa@icloud.com>
2023-04-24 15:15:00 -0300

    feat: add logs to APIgateway

--- lib/ecommerceAPi-stack.ts
+++ lib/ecommerceAPi-stack.ts
@@ -1,7 +1,7 @@
 import * as cdk from 'aws-cdk-lib'
 import * as lambdaNodeJS from 'aws-cdk-lib/aws-lambda-nodejs'
 import * as apigateway from 'aws-cdk-lib/aws-apigateway'
-import * as cwlogs from 'aws-cdk-lib/aws-cloudwatch'
+import * as cwlogs from 'aws-cdk-lib/aws-logs'
 import { Construct } from 'constructs'
 
 interface EcommerceApiStackProps extends cdk.StackProps {
@@ -13,8 +13,24 @@ export class EcommerceApiStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props: EcommerceApiStackProps) {
     super(scope, id, props)
 
+    const logGroup = new cwlogs.LogGroup(this, 'ECommerceApiLogs')
+
     const api = new apigateway.RestApi(this, 'ECommerceApi', {
       restApiName: 'ECommerceApi',
+      deployOptions: {
+        accessLogDestination: new apigateway.LogGroupLogDestination(logGroup),
+        accessLogFormat: apigateway.AccessLogFormat.jsonWithStandardFields({
+          httpMethod: true,
+          ip: true,
+          protocol: true,
+          requestTime: true,
+          resourcePath: true,
+          responseLength: true,
+          status: true,
+          caller: true,
+          user: true
+        })
+      }
     })
 
     const productsFetchIntegration = new apigateway.LambdaIntegration(props.productsFetchFunction)
