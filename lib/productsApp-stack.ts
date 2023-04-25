6fea86b1c7850cfc152a35a73b18e369b24c7d39
Lucas Gonçalves <lucas.floripa@icloud.com>
2023-04-25 15:49:24 -0300

    feat: add new admin handler lambda to productsAppStack

--- lib/productsApp-stack.ts
+++ lib/productsApp-stack.ts
@@ -6,6 +6,7 @@ import { Construct } from 'constructs'
 
 export class ProductsAppStack extends cdk.Stack {
   readonly productsFetchHandler: lambdaNodeJS.NodejsFunction
+  readonly productsAdminHandler: lambdaNodeJS.NodejsFunction
   readonly productsTable: dynamobdb.Table
 
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -23,8 +24,8 @@ export class ProductsAppStack extends cdk.Stack {
       writeCapacity: 1,
     })
 
-    this.productsFetchHandler = new lambdaNodeJS.NodejsFunction(this, 'ProductsFetchHandler', {
-      functionName: 'ProductsFetchHandler',
+    this.productsFetchHandler = new lambdaNodeJS.NodejsFunction(this, 'ProductsFetchFunction', {
+      functionName: 'ProductsFetchFunction',
       entry: 'lambda/products/productsFetchFunction.ts',
       handler: 'handler',
       memorySize: 128,
@@ -39,5 +40,22 @@ export class ProductsAppStack extends cdk.Stack {
     })
 
     this.productsTable.grantReadData(this.productsFetchHandler)
+
+    this.productsAdminHandler = new lambdaNodeJS.NodejsFunction(this, 'ProductsAdminFunction', {
+      functionName: 'ProductsAdminFunction',
+      entry: 'lambda/products/productsAdminFunction.ts',
+      handler: 'handler',
+      memorySize: 128,
+      timeout: cdk.Duration.seconds(5),
+      bundling: {
+        minify: true,
+        sourceMap: false
+      },
+      environment: {
+        PRODUCTS_TABLE_NAME: this.productsTable.tableName
+      }
+    })
+    this.productsTable.grantReadWriteData(this.productsAdminHandler)
+
   }
 }
