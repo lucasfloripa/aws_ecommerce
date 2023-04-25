d914a8e70f69d87af5d39e39bdebb697f2b64da5
Lucas Gonçalves <lucas.floripa@icloud.com>
2023-04-25 16:07:53 -0300

    feat: add new resource handler to productsFetchFunction

--- lambda/products/productsFetchFunction.ts
+++ lambda/products/productsFetchFunction.ts
@@ -1,19 +1,32 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult, Context } from "aws-lambda";
 
-
 export async function handler(event: APIGatewayProxyEvent, context: Context): Promise<APIGatewayProxyResult>{
   const method = event.httpMethod;
   const lambdaRequestId = context.awsRequestId; // identifica unicamente a execução da função lambda
   const apiRequestId = event.requestContext.requestId; // identifica unicamente a requisição feita a API Gateway
 
+  console.log(`API Gateway RequestId: ${apiRequestId} - Lambda RequestId: ${lambdaRequestId}`)
+
   if(event.resource === "/products") {
     if( method === "GET") {
-      console.log("GET")
+      console.log("GET /products")
 
       return {
         statusCode: 200,
         body: JSON.stringify({
           message: 'GET Products - OK'
+        })
+      }
+    }
+  } else if (event.resource === "/products/{id}") {
+    if( method === "GET") {
+      const productId = event.pathParameters!.id as string
+      console.log(`GET /products/${productId}`)
+
+      return {
+        statusCode: 200,
+        body: JSON.stringify({
+          message: 'GET Product ID - OK'
         })
       }
     }
