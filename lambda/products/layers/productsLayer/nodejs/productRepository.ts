45202b6b2348abf827fbab5488fdc53abe413a91
Lucas Gonçalves <lucas.floripa@icloud.com>
2023-05-04 10:41:08 -0300

    feat: productsRepository

---
+++ lambda/products/layers/productsLayer/nodejs/productRepository.ts
@@ -0,0 +1,82 @@
+import { DocumentClient } from 'aws-sdk/clients/dynamodb';
+import { v4 as uuid } from 'uuid'
+
+export interface Product {
+  id: string;
+  productName: string
+  code: string
+  price: number
+  model: string
+}
+
+export class ProductRepository {
+  private ddbClient: DocumentClient
+  private productsTable: string
+
+  constructor(ddbClient: DocumentClient, productsTable: string){
+    this.ddbClient = ddbClient
+    this.productsTable = productsTable
+  }
+
+  getAllProducts = async (): Promise<Product[]> => {
+    const params = {
+      TableName: this.productsTable
+    }
+    const result = await this.ddbClient.scan(params).promise()
+    return result.Items as Product[]
+  }
+
+  getProductById = async (id: string): Promise<Product> => {
+    const params = {
+      TableName: this.productsTable,
+      Key: {
+        id
+      }
+    }
+    const result = await this.ddbClient.get(params).promise()
+    return result.Item as Product
+  }
+
+  createProduct = async (product: Product): Promise<Product> => {
+    product.id = uuid()
+    const params = {
+      TableName: this.productsTable,
+      Item: {
+        ...product
+      }
+    }
+    await this.ddbClient.put(params).promise()
+    return product
+  }
+
+  deleteProduct = async (id: string): Promise<Product> => {
+    const params = {
+      TableName: this.productsTable,
+      Key: {
+        id
+      },
+      ReturnValues: 'ALL_OLD'
+    }
+    const result = await this.ddbClient.delete(params).promise()
+    return result.Attributes as Product
+  }
+
+  updateProduct = async (id: string, product: Product): Promise<Product> => {
+    const params = {
+      TableName: this.productsTable,
+      Key: {
+        id
+      },
+      UpdateExpression: 'set productName = :productName, code = :code, price = :price, model = :model',
+      ExpressionAttributeValues: {
+        ':productName': product.productName,
+        ':code': product.code,
+        ':price': product.price,
+        ':model': product.model
+      },
+      ReturnValues: 'ALL_NEW'
+    }
+    const result = await this.ddbClient.update(params).promise()
+    return result.Attributes as Product
+  }
+}
