dd2f6511abb388c7c73ce7ab57e8099f27c90670
Lucas <lucas.floripa@icloud.com>
2023-05-16 13:55:04 -0300

    feat: add productEventLayer

--- lib/productsAppLayers-stack.ts
+++ lib/productsAppLayers-stack.ts
@@ -4,21 +4,35 @@ import * as ssm from 'aws-cdk-lib/aws-ssm'
 import { Construct } from 'constructs'
 
 export class ProductsAppLayersStack extends cdk.Stack {
-  readonly productsLayer: lambda.LayerVersion
-
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props)
 
-    this.productsLayer = new lambda.LayerVersion(this, 'ProductsLayer', {
+    // LAYER
+    const productsLayer = new lambda.LayerVersion(this, 'ProductsLayer', {
       code: lambda.Code.fromAsset('lambda/products/layers/productsLayer'),
       compatibleRuntimes: [lambda.Runtime.NODEJS_14_X],
       layerVersionName: 'ProductsLayer',
       removalPolicy: cdk.RemovalPolicy.RETAIN
     })
 
+    // SSM
     new ssm.StringParameter(this, 'ProductsLayerVersionArn', {
       parameterName: 'ProductsLayerVersionArn',
-      stringValue: this.productsLayer.layerVersionArn
+      stringValue: productsLayer.layerVersionArn
+    })
+
+    // LAYER
+    const productEventsLayer = new lambda.LayerVersion(this, 'ProductEventsLayer', {
+      code: lambda.Code.fromAsset('lambda/products/layers/productEventsLayer'),
+      compatibleRuntimes: [lambda.Runtime.NODEJS_14_X],
+      layerVersionName: 'ProductEventsLayer',
+      removalPolicy: cdk.RemovalPolicy.RETAIN
+    })
+
+    // SSM
+    new ssm.StringParameter(this, 'ProductEventsLayerVersionArn', {
+      parameterName: 'ProductEventsLayerVersionArn',
+      stringValue: productEventsLayer.layerVersionArn
     })
   }
 }
