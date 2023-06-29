56c48d763e87e49db28517886f0c96d546386632
Lucas Gonçalves <lucas.floripa@icloud.com>
2023-06-29 10:01:41 -0300

    feat: create OrderRepository interfaces

---
+++ lambda/orders/layers/orderEventsRepository/nodejs/orderEventRepository.ts
@@ -0,0 +1,30 @@
+import { DocumentClient } from 'aws-sdk/clients/dynamodb'
+
+export interface OrderEventDdb {
+  pk: string
+  sk: string
+  ttl: number
+  email: string
+  createAt: number
+  requestId: string
+  eventType: string
+  info: {
+    orderId: string
+    productCodes: string[]
+    messageId: string
+  }
+}
+
+export class OrderEventRepository {
+  constructor(
+    private readonly ddbClient: DocumentClient,
+    private readonly eventsDdb: string
+  ) { }
+
+  createOrderEvent(orderEvent: OrderEventDdb) {
+    return this.ddbClient.put({
+      TableName: this.eventsDdb,
+      Item: orderEvent
+    }).promise()
+  }
+}
